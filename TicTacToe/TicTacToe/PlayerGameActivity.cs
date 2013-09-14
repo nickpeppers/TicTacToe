dfe2123629b8d88f65b937116af0c3c114a225e0
Nick Peppers <nick.peppers@hitcents.com>
2013-09-14 17:43:12 -0500

    Made button backgroung clear and larger text
    For some reason when I added the lines to the layout it made the game
    play automatically and I don't know why

--- TicTacToe/TicTacToe/PlayerGameActivity.cs
+++ TicTacToe/TicTacToe/PlayerGameActivity.cs
@@ -9,6 +9,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.Content.PM;
+using Android.Graphics;
 
 namespace TicTacToe
 {
@@ -55,6 +56,8 @@ namespace TicTacToe
 			foreach (var button in _board) 
 			{
 				button.Click += ButtonClick;
+                button.SetBackgroundColor(Color.Transparent);
+                button.SetTextColor(Color.GhostWhite);
 			}
 
 
