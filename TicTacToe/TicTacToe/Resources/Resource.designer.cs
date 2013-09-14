43a4dba857f09130e74c0e7b2b13d5edb186dbaf
Nick Peppers <nick.peppers@hitcents.com>
2013-09-14 18:02:02 -0500

    Added vertical lines (***Bug?***)
    For some reason if I add anything between the button layouts it plays
    on its own da fuq?????

--- TicTacToe/TicTacToe/Resources/Resource.designer.cs
+++ TicTacToe/TicTacToe/Resources/Resource.designer.cs
@@ -66,47 +66,65 @@ namespace TicTacToe
 			// aapt resource value: 0x7f050003
 			public const int PlayerStartButton = 2131034115;
 			
-			// aapt resource value: 0x7f050011
-			public const int RestartButton = 2131034129;
+			// aapt resource value: 0x7f050017
+			public const int RestartButton = 2131034135;
 			
 			// aapt resource value: 0x7f050005
 			public const int Row1 = 2131034117;
 			
-			// aapt resource value: 0x7f050009
-			public const int Row2 = 2131034121;
+			// aapt resource value: 0x7f05000b
+			public const int Row2 = 2131034123;
 			
-			// aapt resource value: 0x7f05000d
-			public const int Row3 = 2131034125;
+			// aapt resource value: 0x7f050011
+			public const int Row3 = 2131034129;
 			
 			// aapt resource value: 0x7f050002
 			public const int TitleText = 2131034114;
 			
+			// aapt resource value: 0x7f050007
+			public const int VerticalLine1 = 2131034119;
+			
+			// aapt resource value: 0x7f050009
+			public const int VerticalLine2 = 2131034121;
+			
+			// aapt resource value: 0x7f05000d
+			public const int VerticalLine3 = 2131034125;
+			
+			// aapt resource value: 0x7f05000f
+			public const int VerticalLine4 = 2131034127;
+			
+			// aapt resource value: 0x7f050013
+			public const int VerticalLine5 = 2131034131;
+			
+			// aapt resource value: 0x7f050015
+			public const int VerticalLine6 = 2131034133;
+			
 			// aapt resource value: 0x7f050006
 			public const int button1 = 2131034118;
 			
-			// aapt resource value: 0x7f050007
-			public const int button2 = 2131034119;
-			
 			// aapt resource value: 0x7f050008
-			public const int button3 = 2131034120;
+			public const int button2 = 2131034120;
 			
 			// aapt resource value: 0x7f05000a
-			public const int button4 = 2131034122;
-			
-			// aapt resource value: 0x7f05000b
-			public const int button5 = 2131034123;
+			public const int button3 = 2131034122;
 			
 			// aapt resource value: 0x7f05000c
-			public const int button6 = 2131034124;
+			public const int button4 = 2131034124;
 			
 			// aapt resource value: 0x7f05000e
-			public const int button7 = 2131034126;
-			
-			// aapt resource value: 0x7f05000f
-			public const int button8 = 2131034127;
+			public const int button5 = 2131034126;
 			
 			// aapt resource value: 0x7f050010
-			public const int button9 = 2131034128;
+			public const int button6 = 2131034128;
+			
+			// aapt resource value: 0x7f050012
+			public const int button7 = 2131034130;
+			
+			// aapt resource value: 0x7f050014
+			public const int button8 = 2131034132;
+			
+			// aapt resource value: 0x7f050016
+			public const int button9 = 2131034134;
 			
 			// aapt resource value: 0x7f050001
 			public const int linearLayout = 2131034113;
